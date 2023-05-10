d1a35ac366abdfdef19a4d37223287d061005995
Soe Sandar Win <soesandarwin2201@gmail.com>
2023-05-10 21:14:44 +0630

    Create the 3d object and greenstock motion

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import { Html, useProgress } from "@react-three/drei"
 
 const Loader = () => {
-  return (
-    <div>Loader</div>
+  const { progress } = useProgress();
+    return (
+    <Html>
+      <span className="canvas-load">
+      </span>
+      <p style={{
+        fontSize: 14,
+        color: '#f1f1f1',
+        fontWeight: 800,
+        marginTop: 40
+      }}>{progress.toFixed(2)}%</p>
+    </Html>
   )
 }
 
--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
 import { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas } from './canvas';
-import Hero from './Hero';
+import Home from './home';
 import Navbar from './Navbar';
 import About from './About';
 import Tech from './Tech';
@@ -9,7 +9,7 @@ import Feedbacks from './Feedbacks';
 import Contact from './Contact';
 
 export {
-  Hero,
+  Home,
   Navbar,
   About,
   Tech,
