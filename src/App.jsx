077d91007ab5befa4a69391dcbbcfd1221ec1424
Soe Sandar Win <soesandarwin2201@gmail.com>
2023-05-10 16:32:32 +0630

    Create the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
-function App() {
+import { BrowserRouter } from "react-router-dom";
+import { About, Contact , Experience, Feedbacks, Hero, Navbar, Tech , Works, StarsCanvas } from './components';
 
+function App() {
   return (
-    <h1>Hello this is my portfolio</h1>
+    <BrowserRouter>
+    <div className="relative z-0 bg-primary">
+      <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+        <Navbar />
+        <Hero />
+      </div>
+      <About />
+      <Experience />
+      <Tech />
+      <Works />
+      <Feedbacks />
+      <div className="relative z-0">
+        <Contact />
+        <StarsCanvas />
+      </div>
+    </div>
+    </BrowserRouter>
   )
 }
 
