da8165db3bf111e51bc805c5705d740a734f8f91
Soe Sandar Win <soesandarwin2201@gmail.com>
2023-05-12 15:20:35 +0630

    Fix the gap problem

--- src/constants/index.js
+++ src/constants/index.js
@@ -129,6 +129,19 @@ import {
        ],
      },
      {
+       title: "React.js Developer",
+       company_name: "Starbucks",
+       icon: starbucks,
+       iconBg: "#383E56",
+       date: "March 2020 - April 2021",
+       points: [
+         "Developing and maintaining web applications using React.js and other related technologies.",
+         "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+         "Implementing responsive design and ensuring cross-browser compatibility.",
+         "Participating in code reviews and providing constructive feedback to other developers.",
+       ],
+     },
+     {
        title: "React Native Developer",
        company_name: "Tesla",
        icon: tesla,
@@ -167,6 +180,32 @@ import {
          "Participating in code reviews and providing constructive feedback to other developers.",
        ],
      },
+     {
+       title: "Full stack Developer",
+       company_name: "Meta",
+       icon: meta,
+       iconBg: "#E6DEDD",
+       date: "Jan 2023 - Present",
+       points: [
+         "Developing and maintaining web applications using React.js and other related technologies.",
+         "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+         "Implementing responsive design and ensuring cross-browser compatibility.",
+         "Participating in code reviews and providing constructive feedback to other developers.",
+       ],
+     },
+     {
+       title: "Full stack Developer",
+       company_name: "Meta",
+       icon: meta,
+       iconBg: "#E6DEDD",
+       date: "Jan 2023 - Present",
+       points: [
+         "Developing and maintaining web applications using React.js and other related technologies.",
+         "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+         "Implementing responsive design and ensuring cross-browser compatibility.",
+         "Participating in code reviews and providing constructive feedback to other developers.",
+       ],
+     },
    ];
    
    const testimonials = [
@@ -185,6 +224,30 @@ import {
        designation: "COO",
        company: "DEF Corp",
        image: "https://randomuser.me/api/portraits/men/5.jpg",
+     },
+     {
+       testimonial:
+         "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+       name: "Lisa Wang",
+       designation: "CTO",
+       company: "456 Enterprises",
+       image: "https://randomuser.me/api/portraits/women/6.jpg",
+     },
+     {
+       testimonial:
+         "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+       name: "Lisa Wang",
+       designation: "CTO",
+       company: "456 Enterprises",
+       image: "https://randomuser.me/api/portraits/women/6.jpg",
+     },
+     {
+       testimonial:
+         "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+       name: "Lisa Wang",
+       designation: "CTO",
+       company: "456 Enterprises",
+       image: "https://randomuser.me/api/portraits/women/6.jpg",
      },
      {
        testimonial:
@@ -237,6 +300,27 @@ import {
          },
        ],
        image: jobit,
+       source_code_link: "https://github.com/",
+     },
+     {
+       name: "Trip Guide",
+       description:
+         "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+       tags: [
+         {
+           name: "nextjs",
+           color: "blue-text-gradient",
+         },
+         {
+           name: "supabase",
+           color: "green-text-gradient",
+         },
+         {
+           name: "css",
+           color: "pink-text-gradient",
+         },
+       ],
+       image: tripguide,
        source_code_link: "https://github.com/",
      },
      {
